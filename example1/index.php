944fdde3887d95b1975add42ae04f37415f6e329
jcfausto <jcfausto@gmail.com>
2012-09-15 12:24:26 -0300

    Finished example
    This example was made in order to experiment iScrol.js version 4
    library. The same example is shown at: http://cubiq.org/iscroll-4

---
+++ example1/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+  	<meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <title>IScroll Example 1</title>
+
+     <!-- Styles -->
+     <link rel="stylesheet" href="css/style.css"/>
+
+    <!-- Scripts -->
+    <script type="text/javascript" src="libs/iscroll.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+  </head>
+  <body>
+  	<div id="header"><a href="http://jcfausto.com" target="_blank">iScroll book list</a></div>
+    <div id="wrapper">
+    	<div id="scroller">
+    	  <ul id="booklist">
+    	  	<li>Book 1</li>
+    	  	<li>Book 2</li>
+    	  	<li>Book 3</li>
+    	  	<li>Book 4</li>
+    	  	<li>Book 5</li>
+    	  	<li>Book 6</li>
+    	  	<li>Book 7</li>
+    	  	<li>Book 8</li>
+    	  	<li>Book 9</li>
+    	  	<li>Book 10</li>
+    	  	<li>Book 11</li>
+    	  	<li>Book 12</li>
+    	  	<li>Book 13</li>
+    	  	<li>Book 14</li>
+    	  	<li>Book 15</li>
+    	  	<li>Book 16</li>
+    	  	<li>Book 17</li>
+    	  	<li>Book 18</li>
+    	  	<li>Book 19</li>
+    	  	<li>Book 20</li>
+    	  	<li>Book 21</li>
+    	  	<li>Book 22</li>
+    	  	<li>Book 23</li>
+    	  	<li>Book 24</li>
+    	  	<li>Book 25</li>
+    	  	<li>Book 26</li>
+    	  	<li>Book 27</li>
+    	  	<li>Book 28</li>
+    	  	<li>Book 29</li>
+    	  	<li>Book 30</li>
+            <li>Book 31</li>
+            <li>Book 32</li>
+            <li>Book 33</li>
+            <li>Book 34</li>
+            <li>Book 35</li>
+            <li>Book 36</li>
+            <li>Book 37</li>
+            <li>Book 38</li>
+            <li>Book 39</li>
+            <li>Book 40</li>
+    	  </ul>	
+    	</div>
+    </div>
+    <div id="footer"><a href="https://github.com/jcfausto/iscroll-examples" target="_blank">Get the source at github</a></div>
+  </body>
+</html>
---
+++ example1/js/app.js
@@ -0,0 +1,44 @@
+/* * * * * * * *
+ * iScroll examples made by Julio Cesar Fausto - 2012.
+ * http://jcfausto.com
+ * http://github.com/jcfausto
+ * Feel free to modify, copy or distribute any piece of this file.
+ */
+ 
+//Global var
+var myScroll;
+
+function loaded() {
+	myScroll = new iScroll('wrapper');
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+/* * * * * * * *
+ *
+ * Use this for high compatibility (iDevice + Android)
+ *
+ */
+document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
+/*
+ * * * * * * * */
+
+
+/* * * * * * * *
+ *
+ * Use this for iDevice only
+ *
+ */
+//document.addEventListener('DOMContentLoaded', loaded, false);
+/*
+ * * * * * * * */
+
+
+/* * * * * * * *
+ *
+ * Use this if nothing else works
+ *
+ */
+//window.addEventListener('load', setTimeout(function () { loaded(); }, 200), false);
+/*
+ * * * * * * * */
